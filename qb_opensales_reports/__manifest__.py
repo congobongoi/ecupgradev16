da565397c8f59304ba16395a9926cf1033f8c0f7
Sahil Navadiya <navadiyasahil@gmail.com>
2024-03-21 18:49:22 +0530

    [ADD]qb_opensales_reports module added

---
+++ qb_opensales_reports/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'EC Group Sales Reports',
+    'description': 'EC Group Open Sales Reports',
+    'sequence': 1,
+    'version': '1.5.0',
+    'author': 'Adam OConnor <aoconnor@quickbeamllc.com>',
+    'website': 'https://quickbeamllc.com',
+    'depends': [
+        'sale_management',
+        'stock',
+        'delivery',
+        'sale_mods_ecgroup',
+        'sale_mrp_link',
+        'sale_stock',
+        'report_xlsx',
+        'qb_sale_purchase_links',
+    ],
+    'data': [
+        'views/sales_report_view.xml',
+        'reports/report_sales.xml',
+        'reports/report_template_sales.xml',
+        'wizard/sales_report_wizard_view.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
